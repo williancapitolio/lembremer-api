806061ae557ac934128872618ca6f87da03eda7b
Willian <williansilvap25@gmail.com>
2023-01-31 23:05:37 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
 import dbConnection from "./database/db.js";
 
 import notesRoute from "./routes/notesRoute.js";
-import priorityRoute from "./routes/prioritiesRoute.js";
+import prioritiesRoute from "./routes/prioritiesRoute.js";
 
 const port = process.env.PORT || 3333;
 const app = express();
 
 dbConnection();
 
+app.use(cors());
 app.use(express.json());
 app.use("/notes", notesRoute);
-app.use("/priorities", priorityRoute);
+app.use("/priorities", prioritiesRoute);
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
